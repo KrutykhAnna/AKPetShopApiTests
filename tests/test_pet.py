993a4e16cbbab1bae77656f0376470e6165af5a0
user <krutykh.anna@icloud.com>
2026-05-09 21:47:49 +0300

    Добавлены тесты: "Попытка обновить несуществующего питомца" "Попытка получить информацию о несуществующем питомце"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -18,3 +18,29 @@ class TestPet:
             assert response.text == "Pet deleted", (f"Ожидаемый результат: text == 'Pet deleted', "
                                                     f" Фактический результат: text == {response.text} ")
 
+    @allure.title("Попытка обновить несуществующего питомца")
+    def test_update_nonexistent_pet(self):
+        with allure.step("Отправка запроса на обновление несуществующего питомца"):
+            body = {
+                "id": random.randrange(1000,10000),
+                "name": "Non-existent Pet",
+                "status": "available"
+            }
+            response = requests.put(url=f"{BASE_URL}/pet", json=body)
+        with allure.step("Проверка status_code"):
+            assert response.status_code == 404, (f"Ожидаемый результат: status_code == 404, "
+                                                 f"Фактический результат: status_code == {response.status_code} ")
+        with allure.step("Проверка текстового содержимого ответа"):
+            assert response.text == "Pet not found", (f"Ожидаемый результат: text == 'Pet not found', "
+                                                    f" Фактический результат: text == {response.text} ")
+
+    @allure.title("Попытка получить информацию о несуществующем питомце")
+    def test_get_nonexistent_pet(self):
+        with allure.step("Отправка запроса на получение информации о несуществующем питомце"):
+            response = requests.get(url=f"{BASE_URL}/pet/{random.randrange(1000,10000)}")
+            with allure.step("Проверка status_code"):
+                assert response.status_code == 404, (f"Ожидаемый результат: status_code == 404, "
+                                                     f"Фактический результат: status_code == {response.status_code} ")
+            with allure.step("Проверка текстового содержимого ответа"):
+                assert response.text == "Pet not found", (f"Ожидаемый результат: text == 'Pet not found', "
+                                                          f" Фактический результат: text == {response.text} ")
