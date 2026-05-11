4dc84fa178690f4eabcf30289a51b8e162475f96
user <krutykh.anna@icloud.com>
2026-05-11 18:12:34 +0300

    Добавлены тесты: 1. "Добавление нового питомца" 2. "Добавление нового питомца c полными данными"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,7 @@
 import allure
+import jsonschema
 import requests
+from schemas.pet_schema import PET_SCHEMA
 
 BASE_URL="http://5.181.109.28:9090/api/v3"
 
@@ -42,3 +44,64 @@ class TestPet:
             with allure.step("Проверка текстового содержимого ответа"):
                 assert response.text == "Pet not found", (f"Ожидаемый результат: text == 'Pet not found', "
                                                           f" Фактический результат: text == {response.text} ")
+
+    @allure.title("Добавление нового питомца")
+    def test_create_new_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            body = {
+                "id": 15,
+                "name": "Rex",
+                "status": "available"
+            }
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=body)
+            response_json = response.json()
+
+        with allure.step("Проверка status_code и валидация JSON-схемы"):
+            assert response.status_code == 200, (f"Ожидаемый результат: status_code == 200, "
+                                                 f"Фактический результат: status_code == {response.status_code} ")
+            jsonschema.validate(response_json,PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца"):
+            assert  response_json["id"] == body["id"], (f"Ожидаемый результат: id == {body["id"]},"
+                                                        f" Фактический результат: id == {response_json["id"]}")
+            assert response_json["name"] == body["name"], (f"Ожидаемый результат: name == {body["name"]},"
+                                                       f" Фактический результат: name == {response_json["name"]}")
+            assert response_json["status"] == body["status"], (f"Ожидаемый результат: status == {body["status"]},"
+                                                       f" Фактический результат: status == {response_json["status"]}")
+
+    @allure.title("Добавление нового питомца c полными данными")
+    def test_create_new_pet(self):
+        with allure.step("Подготовка данных для создания питомца"):
+            body = {
+                "id": 10,
+                "name": "doggie",
+                "category": {"id": 1, "name": "Dogs"},
+                "photoUrls": ["string"],
+                "tags": [{"id": 0, "name": "string"}],
+                "status": "available"
+            }
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=body)
+            response_json = response.json()
+
+        with allure.step("Проверка status_code и валидация JSON-схемы"):
+            assert response.status_code == 200, (f"Ожидаемый результат: status_code == 200, "
+                                                 f"Фактический результат: status_code == {response.status_code} ")
+            jsonschema.validate(response_json, PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца"):
+            assert response_json["id"] == body["id"], (f"Ожидаемый результат: id == {body["id"]},"
+                                                       f" Фактический результат: id == {response_json["id"]}")
+            assert response_json["name"] == body["name"], (f"Ожидаемый результат: name == {body["name"]},"
+                                                           f" Фактический результат: name == {response_json["name"]}")
+            assert response_json["category"] == body["category"], (f"Ожидаемый результат: category == {body["category"]},"
+                                                           f" Фактический результат: category == {response_json["category"]}")
+            assert response_json["photoUrls"] == body["photoUrls"], (f"Ожидаемый результат: photoUrls == {body["photoUrls"]}, "
+                                                                   f"Фактический результат: photoUrls == {response_json["photoUrls"]}")
+            assert response_json["tags"] == body["tags"], (f"Ожидаемый результат: tags == {body["tags"]}, "
+                                                                     f"Фактический результат: tags == {response_json["tags"]}")
+            assert response_json["status"] == body["status"], (f"Ожидаемый результат: status == {body["status"]},"
+                                                               f" Фактический результат: status == {response_json["status"]}")
+
