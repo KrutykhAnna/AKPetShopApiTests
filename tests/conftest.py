c53baa0de0b0749f88dfd85778ac74010cacbc53
user <krutykh.anna@icloud.com>
2026-05-14 21:31:38 +0300

    Добавлены тесты проверяющие работу ручек Store

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,10 +22,10 @@ def create_new_order_fixture():
     """Фикстура создания заказа"""
     body = {
         "id": random.randrange(1,100),
-        "petId": 1,
-        "quantity": 1,
+        "petId": random.randrange(1,100),
+        "quantity":  random.randrange(1,100),
         "status": random.choice(["approved", "placed", "delivered"]),
-        "complete": True}
+        "complete": random.choice([True,False])}
     response = requests.post(url=f"{BASE_URL}/store/order", json=body)
     assert response.status_code == 200
     return response.json()
